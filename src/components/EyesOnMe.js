da3721b14a142a889bbbe9f48ca6d8b3542d8559
kailana <kailanakahawaii@gmail.com>
2020-03-31 11:44:14 -0700

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,27 @@
 // Code EyesOnMe Component Here
+
+import React, { Component } from 'react';
+
+export default class EyesOnMe extends React.Component {
+    constructor(){
+        super()
+
+    }
+
+    focusHandler = (e) => {
+        console.log('Good!')
+    }
+
+    blurHandler = (e) => {
+        console.log('Hey! Eyes on me!')
+    }
+
+
+    render (){
+        console.log(this.props)
+        return (
+            <button onFocus={this.focusHandler} onBlur={this.blurHandler}/>
+        )
+    }
+
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,25 @@
 // Code Keypad Component Here
+
+import React, { Component } from 'react';
+
+export default class Keypad extends React.Component {
+    constructor(){
+        super()
+
+    }
+
+    consoleLog = (event) => {
+        console.log('Entering password...')
+    }
+
+
+    render (){
+        //console.log(this.props)
+        return (
+            <input type="password" onKeyUp={this.consoleLog}/>
+
+          
+        )
+    }
+
+}
